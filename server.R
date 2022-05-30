b999fb510ad0d163060ee3a982308fb95c22ad97
kkenohh <kenoh17@uw.edu>
2022-05-30 16:23:48 -0700

    added necessary files

---
+++ server.R
@@ -0,0 +1,5 @@
+youth_df <- read.csv("YSSBR DATA Small NA.csv")
+
+server <- function(input, output) {
+  
+}
---
+++ ui.R
@@ -0,0 +1,5 @@
+youth_df <- read.csv("YSSBR DATA Small NA.csv")
+
+ui <- navbarPage(
+  
+)
