caf605ba531e8f3aeaf0237cc8b2faa6bf3860c1
wyattsteere <wyattsteere@gmail.com>
2022-05-30 20:03:23 -0700

    Added all three charts from exploratory analysis to shiny page

--- server.R
+++ server.R
@@ -1,5 +1,59 @@
 youth_df <- read.csv("YSSBR DATA Small NA.csv")
+library(tidyverse)
+library(dplyr)
 
 server <- function(input, output) {
-  
+
+  output$age_in_YRBSS_data <- renderPlotly({
+    # create dataframe of ages and add 10 years to all ages
+    #   so that they are accurate
+    ages_df <- youth_df %>% 
+      summarize(age..1..10.years..7.16..years.old. + 10)
+    
+    # makes the ages into a vector 
+    Ages <- ages_df$`age..1..10.years..7.16..years.old. + 10`
+    
+    # plot the ages against the count of students taking the survey
+    ages_in_YSSBR <- ggplot(data = youth_df, aes(x =  Ages)) +
+      geom_bar(stat = "bin", color = "blue", fill = "blue", bins = 7) +
+      ggtitle("Ages Represented in YRBSS Data") +
+      ylab("Number of Survey-Takers") +
+      xlab("Age of Survey-Taker") 
+    ages_in_YSSBR
+    return(ages_in_YSSBR)
+    
+  })
+  output$teen_suicide_viz <- renderPlotly({
+    result <- youth_df %>% 
+      group_by(Year) %>% 
+      summarise(avg_suicide = (mean(q16..Attempted.suicide. - 1, na.rm = TRUE)))
+    
+    teen_suicide_plot <- result %>% 
+      ggplot(aes(x = Year, y = avg_suicide)) +
+      geom_line() +
+      geom_point() +
+      ylab("Proportion of Teens Attempting Suicide 
+       0 = not attempted
+       1 = attempted") + 
+      ggtitle("Proportion of Teens Attempting Suicide Over the Years")
+    teen_suicide_plot
+    return(teen_suicide_plot)
+  })
+  output$race_suicide_viz <- renderPlotly({
+    result_NA <- youth_df %>% 
+      group_by(race4..4.level.race.variable.) %>% 
+      summarise(avg_suicide = (mean(q14..Suicide.consideration. -1, na.rm = T)))
+    result <- head(result_NA, -1)
+    
+    proportion_race_suicide_viz <- result %>% 
+      ggplot(aes(x = race4..4.level.race.variable., y = avg_suicide, color = avg_suicide ,fill = avg_suicide,)) +
+      geom_bar(stat='identity') +
+      ylab("Proportion of Teens Attempting Suicide 
+       1 = White
+       2 = Black
+       3 = Hispanic
+       4 = All other Races") + 
+      ggtitle("Proportion of Teens attempting Suicide by Race")
+    return(proportion_race_suicide_viz)
+  })
 }
--- ui.R
+++ ui.R
@@ -1,5 +1,82 @@
 youth_df <- read.csv("YSSBR DATA Small NA.csv")
 
-ui <- navbarPage(
+library(plotly)
+library("rsconnect")
+library("bslib")
+library("shinythemes")
+library("markdown")
+
+
+
+
+
+# Home page tab
+intro_side_tab <- sidebarPanel(
+  # Title of tab
+  "Introduction",
+  fluidPage(theme = shinytheme("simplex"),
+  )
+)
+main_panel_table <- mainPanel(
+  "Summary",
+  h2("Something about the data"),
   
+  h2("Table viz?"),
+  plotlyOutput(outputId = "race_suicide_viz"),
+  
+)
+intro_tab <- tabPanel(
+  "Intro Tab",
+  sidebarLayout(
+    intro_side_tab,
+    main_panel_table
+  )
+)
+# We want our next tab to have a sidebar layout
+# So we're going to create a sidebarPanel() and a mainPanel() and then add them together
+
+# Create sidebar panel for widget
+sidebar_panel_widget <- sidebarPanel(
+  checkboxGroupInput(
+    inputId = "user_selection",
+    label = "Questions",
+    choices = c("Sample Question 1", "#2", "#3", "#4", "#5"),
+    # True allows you to select multiple choices
+    selected = "Sample Question 1" 
+  ),
+  #sliderInput(inputId = "year_selection",
+  #            label = h3("Slider"),
+  #            min = min(climate_df$year),
+  #            max = max(climate_df$year),
+  #            sep = "",
+  #            value = c(2010, 2020)
+  #)
+)
+
+# Put a plot in the middle of the page
+main_panel_plot <- mainPanel(
+  #  plotOutput(outputId = "climate_plot")
+  # Make plot interactive
+  h2("Spacer"),
+  plotlyOutput(outputId = "teen_suicide_viz"),
+  h2("Sample"),
+  plotlyOutput(outputId = "age_in_YRBSS_data")
+  
+)
+
+# Plot Tab  - combine sidebar panel and main panel
+Question_tab <- tabPanel(
+  "Question Viz",
+  sidebarLayout(
+    sidebar_panel_widget,
+    main_panel_plot
+  )
+)
+
+
+ui <- navbarPage(
+  # Home page title
+  "YSSBR Survey Data",
+  intro_tab,
+  Question_tab
 )
